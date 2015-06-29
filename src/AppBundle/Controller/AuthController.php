94342cfa3fa0f134ee4ac8a68c81d791fc732c6c
Daniel Wolf <daniel.wolf@punchkickinteractive.com>
2015-06-29 11:00:23 -0500

    Working on post /auth route for login.

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -2,18 +2,34 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\LoginType;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
+use FOS\RestBundle\Util\Codes;
 
 class AuthController extends FOSRestController
 {
-    public function postAuthsAction(Request $request)
+    public function postAuthAction(Request $request)
     {
-        $username = $request->request->get('username');
-        $password = $request->request->get('password');
+        $form = $this->createForm(new LoginType());
 
-        $view = $this->view([$username, $password], 200);
-        $view->setTemplate('::default/index.html.twig');
+        $form->handleRequest($request);
+
+        if ($form->isValid() === false) {
+            $view = View::create()
+                ->setStatusCode(Codes::HTTP_BAD_REQUEST)
+                ->setData([
+                    'form' => $form,
+                ]);
+            return $this->handleView($view);
+        }
+
+        // processing
+
+        $view = $this->view(['status' => 'ok'], 201);
         return $this->handleView($view);
+
     }
 }
